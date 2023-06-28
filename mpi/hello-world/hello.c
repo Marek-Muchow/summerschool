5e9f4805893f35bad648f038cbe65bead2b92760
Marek-Muchow <marek.muchow@aalto.fi>
2023-06-28 10:38:22 +0300

    Conducted exercise with hello.c

--- mpi/hello-world/hello.c
+++ mpi/hello-world/hello.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
+#include <mpi.h> // load library 
+
 
 int main(int argc, char *argv[])
 {
+
+    int size, rank, len_name ; // needed for information
+    char processor_name[MPI_MAX_PROCESSOR_NAME] ; 
+
+    MPI_Init(&argc, &argv) ;  // start of MPI
+
+
+    // get rank and size for each process
+    MPI_Comm_size(MPI_COMM_WORLD, &size) ; 
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank) ; 
+
+    // get name of processors 
+    MPI_Get_processor_name(processor_name, &len_name) ; 
+
     // TODO: say hello! in parallel
-    printf("Hello!\n");
+    printf("Hello from %s!\n", processor_name) ;
+
+    if (rank == 0){
+	printf("My rank is %d ", rank) ;
+    	printf("out of %d processes!\n", size) ;
+    }
+    if (rank > 0){
+	printf("My rank is %d!\n", rank) ; 
+    }
+
+
+
+    MPI_Finalize() ; // end of MPI
 }
